33bd1a5dbfac053fad64ae53151d61b677668e34
Vova <rybachykvladimir@gmail.com>
2022-02-16 00:05:34 +0200

    android:windowSoftInputMode="adjustResize"

--- app/src/main/java/com/example/adminpanel/presentation/AdminUserFragment.kt
+++ app/src/main/java/com/example/adminpanel/presentation/AdminUserFragment.kt
@@ -6,14 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.adminpanel.R
+import com.example.adminpanel.databinding.FragmentAdminUserBinding
 
 class AdminUserFragment : Fragment() {
+    private lateinit var binding: FragmentAdminUserBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_user, container, false)
+        binding = FragmentAdminUserBinding.inflate(inflater)
+    return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCancel.setOnClickListener {
+            activity?.onBackPressed()
+        }
     }
 }
